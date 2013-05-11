41d9c7a76daa7f0cc0ee8cd02525ebd5e959161d
chuzui <chuqi77@gmail.com>
2013-05-11 20:55:51 +0800

    cc

--- lan.py
+++ lan.py
@@ -9,6 +9,9 @@ for l in f:
          d[l[i], l[i+1]] = d.get((l[i], l[i+1]), 0) + 1
 
 l = list()
-for key in d.iterkeys():
-    if d[key] > 10:
-        print ''.join(key), d[key]
+item = d.items()
+
+item.sort(lambda x,y: cmp(x[1], y[1]))
+for key, value in item:
+    if value > 10:
+        print ''.join(key), value
